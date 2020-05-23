a6cda1bec06dbf09d0cb64c7098735a30e504eea
Ryan Bowlen <rabowlen@gmail.com>
2020-05-23 13:58:12 -0500

    Start of project

---
+++ src/main/java/com/bowlendevelopment/DWCharacterCreator/DwCharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.bowlendevelopment.DWCharacterCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwCharacterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwCharacterCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bowlendevelopment/DWCharacterCreator/DwCharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowlendevelopment.DWCharacterCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwCharacterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
